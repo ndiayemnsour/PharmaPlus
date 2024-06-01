b5630f21a4ca0816133b203351e98ad7cdf11710
ndiayemnsour <zocod3r@gmail.com>
2024-06-01 17:11:06 +0000

    Interface Produit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-ads:23.1.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.android.car.ui:car-ui-lib:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/sn/unchk/pharmaplus/ProduitViewModel.kt
@@ -0,0 +1,7 @@
+package sn.unchk.pharmaplus
+
+import androidx.lifecycle.ViewModel
+
+class ProduitViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
